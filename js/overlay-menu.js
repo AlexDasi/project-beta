e7771bdf358fa3ca388cfa7b4c15163fadf5eab5
AlexDasi <alejandrodasi@gmail.com>
2022-03-18 17:27:44 +0100

    no funciona

--- js/overlay-menu.js
+++ js/overlay-menu.js
@@ -7,3 +7,7 @@ function openNav() {
   function closeNav() {
     document.getElementById("myNav").style.width = "0%";
   }
+
+  $('button').on('click', function(){
+    $('body').toggleClass('open');
+  });
